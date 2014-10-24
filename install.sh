f77c744d59be38b143377d6577ca1fdf3b0545b1
Jason Draper <jason@drapergeek.com>
2014-10-24 15:58:42 -0400

    Add setup

--- install.sh
+++ install.sh
@@ -9,6 +9,3 @@ fi
 
 rcup -d $TB_DOTFILES -x README.md -x LICENSE -x Brewfile -x rcrc
 rcup -f -d $MY_DOTFILES -x README.md -x Brewfile -x install
-
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim -u ~/.vimrc.bundles +BundleInstall +qa
