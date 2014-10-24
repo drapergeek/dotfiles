a4760faff31b10503fe8b8230006815b68f0a6aa
Jason Draper <jason@drapergeek.com>
2014-10-24 15:29:55 -0400

    Remove duplicates from thoughtbot dotfiles

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim -u ~/.vimrc.bundles +BundleInstall +qa
-
-MY_DOTFILES=$HOME/.dotfiles
-TB_DOTFILES=$HOME/.dotfiles-thoughtbot
+MY_DOTFILES=$HOME/dotfiles
+TB_DOTFILES=$HOME/dotfiles-thoughtbot
 
 if [ ! -e $TB_DOTFILES ]; then
   git clone https://github.com/thoughtbot/dotfiles.git $TB_DOTFILES
@@ -12,3 +9,6 @@ fi
 
 rcup -d $TB_DOTFILES -x README.md -x LICENSE -x Brewfile -x rcrc
 rcup -f -d $MY_DOTFILES -x README.md -x Brewfile -x install
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim -u ~/.vimrc.bundles +BundleInstall +qa
