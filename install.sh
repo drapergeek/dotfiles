f2020f30023554e7e4f46d9fa7b25471f5507322
Jason Draper <jason@drapergeek.com>
2018-01-14 13:29:55 -0500

    Add a basic libinput conf

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 MY_DOTFILES=$HOME/dotfiles
 
-rcup -d $MY_DOTFILES -x README.md -x Brewfile -x install -x rcrc -x License
+rcup -d $MY_DOTFILES -x README.md -x Brewfile -x install -x rcrc -x License -x 90-libinput.conf
+sudo ln -s /home/draper/dotfiles/90-libinput.conf /usr/share/X11/xorg.conf.d/90-libinput.conf
+
