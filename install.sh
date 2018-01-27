5501b0eaba8e6b6da3acd890fbdf4e264559019a
Jason Draper <jason@drapergeek.com>
2018-01-26 22:32:53 -0500

    Move config files over to Manjaro setup
    Most of this came from the default Manjaro i3 setup
    For the most part, I'm trying to accept the defaults where I can
    and not change unless I truly find I like the "old way" better.
    
    * Setup tmux copy paste correctly
    * Setup basic workstation configuration I had on the previous setup
    * Setup the montior resolution
    * Use the default Xresources and gtk for manjaro. As I see fit, I'm sure
    I'll change things.

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@
 
 MY_DOTFILES=$HOME/dotfiles
 
-rcup -d $MY_DOTFILES -x README.md -x Brewfile -x install -x rcrc -x License -x 90-libinput.conf
-sudo ln -s /home/draper/dotfiles/90-libinput.conf /usr/share/X11/xorg.conf.d/90-libinput.conf
+rcup -d $MY_DOTFILES \
+	-x README.md  \
+	-x Brewfile \
+	-x install \
+	-x rcrc \
+	-x License \
+	-x 90-libinput.conf \
+	-x compton.conf \
+	-x gtkrc-2.0 \
+	-x screenlayout \
+	-x Xresources \
+	-x config/gtk-3.0 \
 
+
+# sudo ln -s /home/draper/dotfiles/90-libinput.conf /usr/share/X11/xorg.conf.d/90-libinput.conf
