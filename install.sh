b7b369ed8f0eae851fe3a3af551355cb1a9e208f
Jason Draper <jason@drapergeek.com>
2018-01-28 20:25:23 -0500

    Include the important stuff

--- install.sh
+++ install.sh
@@ -9,11 +9,6 @@ rcup -d $MY_DOTFILES \
 	-x rcrc \
 	-x License \
 	-x 90-libinput.conf \
-	-x compton.conf \
-	-x gtkrc-2.0 \
-	-x screenlayout \
-	-x Xresources \
-	-x config/gtk-3.0 \
 
 
 # sudo ln -s /home/draper/dotfiles/90-libinput.conf /usr/share/X11/xorg.conf.d/90-libinput.conf
