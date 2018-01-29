a3b3bf9b3eb5b4ff63cdad55e4349713c8e370ac
Jason Draper <jason@drapergeek.com>
2018-01-29 09:14:48 -0500

    Fix monitor layout in i3 config

---
+++ screenlayout/work_dual_monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x248 --rotate normal --output DP1 --off --output HDMI2 --mode 2560x1440 --pos 1920x48 --rotate normal --output HDMI1 --off --output DP2 --off
