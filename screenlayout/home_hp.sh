9458582cd511fa7f32846b7bfaa14c5d2ab14167
Jason Draper <jason@drapergeek.com>
2018-07-08 11:42:55 -0400

    Add new screenlayouts

---
+++ screenlayout/home_hp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output DP-2 --primary --mode 2560x1440 --scale 2x2 --pos 3840x0 --rotate normal
+xrandr --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal
---
+++ screenlayout/hp.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-4 --off --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal
