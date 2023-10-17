6473e455ef2577d2cb07bd4689fedea207247872
Jason Draper <jason@drapergeek.com>
2023-10-17 14:34:32 -0400

    Add brew bundle to the install

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ rcup -d $MY_DOTFILES \
 	-x Brewfile \
 	-x install \
 	-x rcrc \
-	-x 90-libinput.conf \
-
+	-x 90-libinput.conf
+brew bundle
 
 # sudo ln -s /home/draper/dotfiles/90-libinput.conf /usr/share/X11/xorg.conf.d/90-libinput.conf
