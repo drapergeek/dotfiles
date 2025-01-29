a906be320e92b061af8dfabbd06da0bcc8644b46
Jason Draper <jason@drapergeek.com>
2025-01-28 21:31:24 -0500

    Add tpm

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 MY_DOTFILES=$HOME/dotfiles
 
 rcup -d $MY_DOTFILES \
-	-x README.md  \
-	-x Brewfile \
-	-x install \
-	-x rcrc \
-	-x 90-libinput.conf
+  -x README.md \
+  -x Brewfile \
+  -x install \
+  -x rcrc \
+  -x 90-libinput.conf
 brew bundle
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # sudo ln -s /home/draper/dotfiles/90-libinput.conf /usr/share/X11/xorg.conf.d/90-libinput.conf
